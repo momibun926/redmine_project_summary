167b1b61a6cef7b59ab26bead22deb459df6edd5
Hajime <hajime.nakagama@gmail.com>
2016-01-30 09:45:00 +0900

    try

--- app/controllers/project_summary_controller.rb
+++ app/controllers/project_summary_controller.rb
@@ -8,36 +8,25 @@ class ProjectSummaryController < ApplicationController
     @issues = @project.issues
 
     @tracker_count = {}
-    @tracker_status_count = {}
-    @tracker_user_count = {}
     @status_count = {}
     @category_count = {}
     @user_count = {}
 
     @issues.each do |issue|
       # tracker
-      @tracker_count[issue.tracker_id] = inc_hash(@tracker_count[issue.tracker_id])
-      # tracker-status
-      @tracker_status_count[ [issue.tracker_id, issue.status_id] ] = inc_hash(@tracker_status_count[ [issue.tracker_id, issue.status_id] ])
-      # tracker-user
-      @tracker_user_count[ [issue.tracker_id, issue.assigned_to_id] ] = inc_hash(@tracker_user_count[ [issue.tracker_id, issue.assigned_to_id] ])
-      # status
-      @status_count[issue.status_id] = inc_hash(@status_count[issue.status_id])
-      @category_count[issue.category_id].nil? ? @category_count[issue.category_id] = 1 : @category_count[issue.category_id] += 1
-      @user_count[issue.assigned_to_id].nil? ? @user_count[issue.assigned_to_id] = 1 : @user_count[issue.assigned_to_id] += 1
+      @tracker_count[issue.tracker.name].nil? ? @tracker_count[issue.tracker.name] = 1 : @tracker_count[issue.tracker.name] += 1
+      @status_count[issue.status.name].nil? ?  @status_count[issue.status.name] = 1 : @status_count[issue.status.name] += 1
+      @category_count[issue.category].nil? ? @category_count[issue.category] = 1 : @category_count[issue.category] += 1
+      @user_count[issue.assigned_to.login].nil? ? @user_count[issue.assigned_to.login] = 1 : @user_count[issue.assigned_to.login] += 1
     end
-    
+
     @tracker_count
-    @tracker_status_count
-    @tracker_user_count
     @status_count
     @category_count
     @user_count
 
   end
-  def inc_hash count_hash
-    count_hash.nil? ? 1 : count_hash + 1
-  end
+
   def find_project
     @project = Project.find(params[:project_id])
   rescue ActiveRecord::RecordNotFound
