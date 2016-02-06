b446dc673a34fd95ec3debd0a7f9f8fc71a60b51
Hajime <hajime.nakagama@gmail.com>
2016-02-06 21:03:42 +0900

    add graph

--- app/controllers/project_summary_controller.rb
+++ app/controllers/project_summary_controller.rb
@@ -11,19 +11,35 @@ class ProjectSummaryController < ApplicationController
     @status_count = {}
     @category_count = {}
     @user_count = {}
+    @timeline = {}
 
     @issues.each do |issue|
       # tracker
-      @tracker_count[issue.tracker.name].nil? ? @tracker_count[issue.tracker.name] = 1 : @tracker_count[issue.tracker.name] += 1
-      @status_count[issue.status.name].nil? ?  @status_count[issue.status.name] = 1 : @status_count[issue.status.name] += 1
-      @category_count[issue.category].nil? ? @category_count[issue.category] = 1 : @category_count[issue.category] += 1
-      @user_count[issue.assigned_to.login].nil? ? @user_count[issue.assigned_to.login] = 1 : @user_count[issue.assigned_to.login] += 1
+      tracker_name = issue.tracker.name
+      unless tracker_name.blank?
+        @tracker_count[tracker_name].nil? ? @tracker_count[tracker_name] = 1 : @tracker_count[tracker_name] += 1
+      end
+      # status
+      status_name = issue.status.name
+      unless status_name.blank?
+        @status_count[status_name].nil? ?  @status_count[status_name] = 1 : @status_count[status_name] += 1
+      end
+      # category
+      cateory_name = issue.category
+      unless cateory_name.blank?
+        @category_count[cateory_name].nil? ? @category_count[cateory_name] = 1 : @category_count[cateory_name] += 1
+      end
+      # assignee
+      unless issue.assigned_to.blank?
+        user_name = issue.assigned_to.login
+        @user_count[user_name].nil? ? @user_count[user_name] = 1 : @user_count[user_name] += 1
+      end
+      # start-date
+      unless issue.start_date.blank?
+        start_date = issue.start_date.to_time(:local).to_i * 1000
+        @timeline[start_date].nil? ? @timeline[start_date] = 1 : @timeline[start_date] += 1
+      end
     end
-
-    @tracker_count
-    @status_count
-    @category_count
-    @user_count
 
   end
 
--- assets/javascripts/drawchart.js
+++ assets/javascripts/drawchart.js
@@ -1,7 +1,10 @@
-function drawTrackerChart(chartdata, placeholder){ 
+function drawPieChart(chartdata, placeholder){
     var chartOptions = {
             credits:{
                 enabled: false
+            },
+            title: {
+                text: ''
             },
             chart: {
                 type: 'pie',
@@ -19,11 +22,43 @@ function drawTrackerChart(chartdata, placeholder){
                         format: '<b>{point.name}</b>: {point.percentage:.1f} %'
                     }
                 }
-            },            
+            },
             series:[{
                 data: chartdata
             }]
       };
       var lg1 = new Highcharts.Chart(chartOptions);
 }
-
+function drawTimelineChart(chartdata, placeholder){
+  var chartOptions = {
+          credits:{
+              enabled: false
+          },
+          chart:{
+              renderTo: placeholder,
+              type: 'line',
+          },
+          title:{
+              text: '',
+              align: 'left'
+          },
+          xAxis:{
+              type: 'datetime'
+          },
+          legend:{
+              align: 'center',
+              verticalAlign: 'bottom',
+              borderWidth: 0,
+              itemDistance: 50
+          },
+          series:[
+              {
+                name: 'Create on',
+                color: '#0f75bc',
+                data: chartdata
+              }
+          ]
+    };
+    Highcharts.setOptions({global : {useUTC : false}});
+    var lg1 = new Highcharts.Chart(chartOptions);
+}
